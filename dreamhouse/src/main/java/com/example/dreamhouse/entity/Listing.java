0fbb77893f205756c3db3cb06b668bd45737fcc4
CiprianTXT <mc.popescu2002@gmail.com>
2025-04-16 00:51:05 +0300

    Specified database schema for entity

--- dreamhouse/src/main/java/com/example/dreamhouse/entity/Listing.java
+++ dreamhouse/src/main/java/com/example/dreamhouse/entity/Listing.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "listings")
+@Table(name = "listings", schema = "project")
 public class Listing {
     @Id
     @GeneratedValue
--- dreamhouse/src/main/java/com/example/dreamhouse/entity/User.java
+++ dreamhouse/src/main/java/com/example/dreamhouse/entity/User.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "project")
 public class User {
 
     @Id
