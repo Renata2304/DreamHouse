1cbae7a0adc8a405ac3398826b27e2651416a70f
CiprianTXT <mc.popescu2002@gmail.com>
2025-05-11 18:39:45 +0300

    Renamed main Spring files

--- dreamhouse-user-service/src/main/java/com/example/dreamhouse/DreamhouseUserApplication.java
+++ dreamhouse-user-service/src/main/java/com/example/dreamhouse/DreamhouseUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DreamhouseApplication {
+public class DreamhouseUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DreamhouseApplication.class, args);
+        SpringApplication.run(DreamhouseUserApplication.class, args);
     }
 
 }
