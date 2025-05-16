c5c3d9e2a10ef58f7edff5ebe4982bf46bf61e15
CiprianTXT <mc.popescu2002@gmail.com>
2025-05-16 03:46:25 +0300

    Added backend metrics support

--- dreamhouse-listing-service/src/main/java/com/example/dreamhouse/security/SecurityConfig.java
+++ dreamhouse-listing-service/src/main/java/com/example/dreamhouse/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
                                         "/v3/api-docs",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html",
-                                        "/listing/getByLocation"
+                                        "/listing/getByLocation",
+                                        "/actuator/prometheus"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 );
