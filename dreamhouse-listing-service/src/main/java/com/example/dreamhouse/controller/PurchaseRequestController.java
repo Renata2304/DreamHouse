0eccfe073d48402f52a500986575d5298d9b7eab
CiprianTXT <mc.popescu2002@gmail.com>
2025-05-18 16:26:28 +0300

    Renamed file and class declarations

--- dreamhouse-listing-service/src/main/java/com/example/dreamhouse/controller/PurchaseRequestController.java
+++ dreamhouse-listing-service/src/main/java/com/example/dreamhouse/controller/PurchaseRequestController.java
@@ -4,17 +4,14 @@ import com.example.dreamhouse.entity.PurchaseRequest;
 import com.example.dreamhouse.entity.User;
 import com.example.dreamhouse.service.PurchaseRequestService;
 import com.example.dreamhouse.service.UserService;
-import com.example.dreamhouse.service.dto.PurchaseRequestDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
