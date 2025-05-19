6007a281f1e8200d6488a2600eff543bc76202b6
CiprianTXT <mc.popescu2002@gmail.com>
2025-05-20 01:35:33 +0300

    Fixed wrong type

--- dreamhouse-feedback-service/src/main/java/com/example/dreamhouse/repository/FeedbackRepository.java
+++ dreamhouse-feedback-service/src/main/java/com/example/dreamhouse/repository/FeedbackRepository.java
@@ -4,6 +4,8 @@ import com.example.dreamhouse.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+public interface FeedbackRepository extends JpaRepository<Feedback, UUID> {
 } 
