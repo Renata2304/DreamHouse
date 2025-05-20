5cb2baf6580a9c1abee529814026382ef641176f
Renata <94386250+Renata2304@users.noreply.github.com>
2025-05-20 14:40:25 +0300

    Update Feedback Service DTO

--- dreamhouse-feedback-service/src/main/java/com/example/dreamhouse/service/dto/FeedbackRequest.java
+++ dreamhouse-feedback-service/src/main/java/com/example/dreamhouse/service/dto/FeedbackRequest.java
@@ -1,17 +1,40 @@
 package com.example.dreamhouse.service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class FeedbackRequest {
     private String satisfactionLevel;
     private String category;
     private String comments;
     private boolean wantUpdates;
-} 
+
+    public String getSatisfactionLevel() {
+        return satisfactionLevel;
+    }
+
+    public void setSatisfactionLevel(String satisfactionLevel) {
+        this.satisfactionLevel = satisfactionLevel;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public boolean isWantUpdates() {
+        return wantUpdates;
+    }
+
+    public void setWantUpdates(boolean wantUpdates) {
+        this.wantUpdates = wantUpdates;
+    }
+}
