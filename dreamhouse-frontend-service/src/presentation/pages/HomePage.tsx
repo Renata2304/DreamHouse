d7bae368d8e1ca19774f8874e25cc1fa1a3d15ce
CiprianTXT <mc.popescu2002@gmail.com>
2025-05-21 15:54:43 +0300

    Copied assets to docker image

--- dreamhouse-frontend-service/src/presentation/pages/HomePage.tsx
+++ dreamhouse-frontend-service/src/presentation/pages/HomePage.tsx
@@ -3,19 +3,19 @@ import { Fragment, memo } from "react";
 import { useIntl } from "react-intl";
 import { Seo } from "@presentation/components/ui/Seo";
 import { Box, Typography } from "@mui/material";
-import downloadSvg from "../../assets/download(1).svg";
+import downloadSvg from "../../assets/download.svg";
 
 export const HomePage = memo(() => {
   const { formatMessage } = useIntl();
-
+  
   return (
     <Fragment>
       <Seo title="DreamHouse | Home" />
       <WebsiteLayout>
         <Box className="px-[50px] py-12 flex flex-col items-center justify-center space-y-6">
-          <img 
-            src="/src/assets/download.svg" 
-            alt="DreamHouse Logo" 
+          <img
+            src={downloadSvg}
+            alt="DreamHouse Logo"
             className="w-48 h-48 object-contain"
           />
           <Typography variant="h3" component="h1" className="text-center">
