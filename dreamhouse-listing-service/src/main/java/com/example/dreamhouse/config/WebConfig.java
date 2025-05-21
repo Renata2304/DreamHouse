ea0f5aaf8b7b9ee75a79f5e5145eabcf69a0fe6f
Renata <94386250+Renata2304@users.noreply.github.com>
2025-05-22 02:12:47 +0300

    Added the CORS WebConfig files

--- dreamhouse-listing-service/src/main/java/com/example/dreamhouse/config/WebConfig.java
+++ dreamhouse-listing-service/src/main/java/com/example/dreamhouse/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.dreamhouse.security;
+package com.example.dreamhouse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
